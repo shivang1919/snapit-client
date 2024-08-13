8d3c0809ba39cb8ed4a18dfd0364e4a66241d1b4
shivang <shivangnov@gmail.com>
2024-08-13 12:51:12 +0530

    Added UI changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
+const inter = Inter({ subsets: ["latin"] });
 import type { AppProps } from "next/app";
-
+import { Inter,Quicksand } from "next/font/google";
+const quickSand = Quicksand({subsets:["latin"]})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={inter.className}>
+   <Component {...pageProps} />;
+   </div>
+  )
 }
